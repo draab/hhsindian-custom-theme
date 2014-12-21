487132d7ecf50d0cafbaa76fc7107e17ef148bec
David Raab <davidraab@outlook.com>
2014-12-21 11:16:35 -0500

    Figured out how the loop formats archives pages correctly.
    Now the real work begins using PHP and various Internet resources.

--- page-archives.php
+++ page-archives.php
@@ -1,12 +1,7 @@
 <!-- Code that runs the date archive page -->
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other 'pages' on your WordPress site will use a different template.
- *
+ * This will override the default page constructor for the twenty fourteen theme. This way the page template does not have to be defined manually.
  * @package WordPress
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
@@ -25,7 +20,12 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<h1>Site Wide Archives</h1>
+			<article id="post-7" class="post-7 page type-page status-publish hentry">
+				<header class="entry-header"><h1 class="entry-title">The Indian's Archives</h1></header>
+					<div class="entry-content">
+					<!-- This is where php code will go to list site archives -->
+					</ul>
+			</article>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->

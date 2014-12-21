994a951c870d72389eeb4ece206cfee9878c7331
David Raab <davidraab@outlook.com>
2014-12-21 12:35:13 -0500

    made a working archive by school year.
    right now it is powered by manually adding a tag.
    
    It's almost indentical to how "featured" posts are decided upon.

--- page-archives.php
+++ page-archives.php
@@ -20,13 +20,35 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<article id="post-7" class="post-7 page type-page status-publish hentry">
+			<article>
 				<header class="entry-header"><h1 class="entry-title">The Indian's Archives</h1></header>
 					<div class="entry-content">
-					<!-- This is where php code will go to list site archives -->
-					</ul>
+				<h1>Stories</h1>
+					<div id="archivelist">
+						<ul>
+						<?php
+ 
+						// The Query
+						query_posts( array ( 'tag' => '2013-2014', 'posts_per_page' => -1 ) );
+ 
+						// The Loop
+						while ( have_posts() ) : the_post(); ?>
+							<li>
+							<em><?php the_time('M'); ?> <?php the_time('j'); ?> <?php the_time('Y'); ?></em> &nbsp; <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a> (<?php the_author();?>)
+							</li>
+ 
+						<?php endwhile;
+ 
+						// Reset Query
+						wp_reset_query();
+ 
+						?>
+						</ul>
+					</div>
+				<h1>Authors</h1>
+				<?php wp_list_authors('show_fullname=1&optioncount=1&orderby=post_count&order=DESC&exclude=1'); ?>
+				<p></p>
 			</article>
-
 		</div><!-- #content -->
 	</div><!-- #primary -->
 	<?php get_sidebar( 'content' ); ?>
